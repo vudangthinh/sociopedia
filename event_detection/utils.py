0b7a51dcefd871dbf857d2a2f6459474b36642f4
Thinh <thinhbn2408@gmail.com>
2020-09-15 15:23:05 +0900

    fix start_date end_date is None

--- event_detection/utils.py
+++ event_detection/utils.py
@@ -141,12 +141,12 @@ def paging_tweets(tweet_list, page):
     return tweets, tweet_index, page_range
 
 def get_tweet_in_time_range(pk, start_date, end_date):
-    if start_date is None or start_date == "":
+    if start_date is None or start_date == "" or start_date == "None":
         start_date = datetime.strptime("1970-01-01 00:00", "%Y-%m-%d %H:%M")
     else:
         start_date = datetime.strptime(start_date, "%Y-%m-%d %H:%M")
 
-    if end_date is None or end_date == "":
+    if end_date is None or end_date == "" or end_date == "None":
         end_date = timezone.now()
     else:
         end_date = datetime.strptime(end_date, "%Y-%m-%d %H:%M")
